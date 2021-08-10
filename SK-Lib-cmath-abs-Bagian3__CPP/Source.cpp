55646f0d11dc5d7e49705056aece29353527cf2c
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2021-08-10 21:16:21 +0700

    Add project files.

---
+++ SK-Lib-cmath-abs-Bagian3__CPP/Source.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+#include <conio.h>
+
+/*
+    Source by Programiz
+    Modified For Learn by RK
+    I.D.E : VS2019
+*/
+
+using namespace std;
+
+int main() {
+    double x = -43.12, hasil;
+
+    hasil = abs(x);
+    cout << "abs(" << x << ") = |" << x << "| = " << hasil << endl;
+
+    _getch();
+    return 0;
+}
